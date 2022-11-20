3723ed530b34065e4eb866f1702ab80c26d98652
Luis Paulo de Lima Soares <pls.luis@gmail.com>
2022-11-20 17:51:23 -0300

    Renderização condicional

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,9 @@
       <FormularioVue @aoSalvarTarefa="salvarTarefa"/>
       <div class="lista">
         <TarefaVue v-for="(tarefa, index) in tarefas" :key="index" :tarefa="tarefa" />
+        <BoxVue v-if="listaEstaVazia">
+          Você ainda não iniciou nenhuma tarefa hoje! :P 
+        </BoxVue>
       </div>
     </div>
   </main>
@@ -18,17 +21,24 @@ import BarraLateral from './components/BarraLateral.vue';
 import FormularioVue from './components/Formulario.vue';
 import TarefaVue from './components/Tarefa.vue';
 import ITarefa from './interfaces/ITarefa';
+import BoxVue from './components/Box.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
     BarraLateral,
     FormularioVue,
-    TarefaVue
+    TarefaVue,
+    BoxVue
   },
   data() {
     return {
       tarefas: [] as ITarefa[]
+    }
+  },
+  computed: {
+    listaEstaVazia() : boolean {
+      return this.tarefas.length === 0;
     }
   },
   methods: {
@@ -41,6 +51,6 @@ export default defineComponent({
 
 <style>
   .lista {
-    padding: 1,25rem;
+    padding: 1.25rem;
   }
 </style>
