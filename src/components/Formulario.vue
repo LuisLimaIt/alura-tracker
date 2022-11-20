6afd634fd1ef31abaf5171d538a5a19bbdf8c25f
Luis Paulo de Lima Soares <pls.luis@gmail.com>
2022-11-20 16:07:41 -0300

    Evoluindo o temporizador

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,64 +1,24 @@
 <template>
     <div class="box">
         <div class="columns">
-            <div 
-                class="column is-8" 
-                role="form" 
-                aria-label="Formulário para criação de uma nova tarefa"
-            >
-                <input 
-                    type="text" 
-                    class="input" 
-                    placeholder="Qual tarefa você deseja iniciar?" 
-                />
+            <div class="column is-8" role="form" aria-label="Formulário para criação de uma nova tarefa">
+                <input type="text" class="input" placeholder="Qual tarefa você deseja iniciar?" />
             </div>
-
             <div class="column">
-                <div class="is-flex is-align-items-center is-justify-content-space-between">
-                    <CronometroVue :tempoEmSegundos="tempoEmSegundos" />
-                    <button class="button" @click="iniciar">
-                        <span class="icon">
-                            <i class="fas fa-play"></i>
-                        </span>
-                        <span>play</span>
-                    </button>
-                    <button class="button" @click="finalizar">
-                        <span class="icon">
-                            <i class="fas fa-stop"></i>
-                        </span>
-                        <span>stop</span>
-                    </button>
-                </div>
+                <TemporizadorVue />
             </div>
         </div>
     </div>
 </template>
 
 <script lang="ts">
-    import { defineComponent } from 'vue';
-    import CronometroVue from './Cronometro.vue';
+import { defineComponent } from 'vue';
+import TemporizadorVue from './Temporizador.vue';
 
-    export default defineComponent({
-        name: 'FormularioVue',
-        components: {
-            CronometroVue
-        },
-        data() {
-            return {
-                tempoEmSegundos: 0,
-                cronometro: 0
-            }
-        },
-        methods: {
-            iniciar() {
-                //Começar a contagem
-                this.cronometro = setInterval(() => { 
-                    this.tempoEmSegundos++
-                }, 1000);
-            },
-            finalizar() {
-                clearInterval(this.cronometro);
-            }
-        }
-    });
+export default defineComponent({
+    name: 'FormularioVue',
+    components: {
+        TemporizadorVue
+    }
+});
 </script>
