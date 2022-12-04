c6028e95396752590582db3fb8d427092c77ed32
Luis Paulo de Lima Soares <pls.luis@gmail.com>
2022-12-03 21:03:14 -0300

    Modificando o estado

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,12 @@
 import IProjeto from "@/interfaces/IProjeto";
 import { InjectionKey } from "vue";
-import { createStore } from "vuex";
+import { createStore, Store } from "vuex";
 
 interface Estado {
     projetos: IProjeto[];
 }
 
-export const key: InjectionKey<Estado> = Symbol()
+export const key: InjectionKey<Store<Estado>> = Symbol()
 
 export const store = createStore<Estado>({
     state: {
