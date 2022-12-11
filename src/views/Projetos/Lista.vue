914d5832ab4f4c5d20646e29b868dd72b71c335c
Luis Paulo de Lima Soares <pls.luis@gmail.com>
2022-12-11 16:35:56 -0300

    Excluindo projetos e organizando as mutations

--- src/views/Projetos/Lista.vue
+++ src/views/Projetos/Lista.vue
@@ -24,6 +24,11 @@
                   <i class="fas fa-pencil-alt"></i>
                 </span>
               </router-link>
+              <button class="button ml-2 is-danger" @click="excluir(projeto.id)">
+                <span class="icon is-small">
+                    <i class="fas fa-trash"></i>
+                </span>
+              </button>
             </td>
           </tr>
         </tbody>
@@ -35,13 +40,20 @@
   import { defineComponent } from 'vue';
   import { useStore } from '@/store';
   import { computed } from '@vue/reactivity';
+  import { EXCLUI_PROJETO } from '@/store/tipo-mutacoes'
   
   export default defineComponent({
     name: 'ListaVue',
+    methods: {
+        excluir (id: string) {
+            this.store.commit(EXCLUI_PROJETO, id)
+        }
+    },
     setup() {
       const store = useStore()
       return {
-        projetos: computed(() => store.state.projetos)
+        projetos: computed(() => store.state.projetos),
+        store
       }
     }
   });
