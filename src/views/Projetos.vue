c188c45e4ca72568318e5a625188fe6fb9e08390
Luis Paulo de Lima Soares <pls.luis@gmail.com>
2022-12-11 15:00:38 -0300

    Rotas aninhadas

--- src/views/Projetos.vue
+++ src/views/Projetos.vue
@@ -1,51 +1,18 @@
 <template>
-  <section class="projetos">
+  <div class="projetos">
     <h1 class="title">Projetos</h1>
-    <router-link to="/projetos/novo" class="button">
-      <span class="icon is-small">
-        <i class="fas fa-plus"></i> 
-      </span>
-      <span>Novo projeto</span>
-    </router-link>
-    <table class="table is-fullwidth">
-      <thead>
-        <tr>
-          <th>Id</th>
-          <th>Nome</th>
-          <th>Ações</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="projeto in projetos" :key="projeto.id">
-          <td>{{projeto.id}}</td>
-          <td>{{projeto.name}}</td>
-          <td>
-            <router-link :to="`/projetos/${projeto.id}`" class="button">
-              <span class="icon is-small">
-                <i class="fas fa-pencil-alt"></i>
-              </span>
-            </router-link>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </section>
+    <router-view />
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import { useStore } from '@/store';
-import { computed } from '@vue/reactivity';
 
-export default defineComponent({
-  name: 'ProjetosVue',
-  setup() {
-    const store = useStore()
-    return {
-      projetos: computed(() => store.state.projetos)
-    }
-  }
-});
+  import { defineComponent } from "vue";
+
+  export default defineComponent({
+    name: 'ProjetosVue'
+  });
+
 </script>
 
 <style scoped>
