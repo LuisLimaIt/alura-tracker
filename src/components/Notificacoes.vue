cb5d3498211d03c6e1ed34f693db4ca049bb6207
Luis Paulo de Lima Soares <pls.luis@gmail.com>
2022-12-31 14:41:09 -0300

    Estrutura inicial do componente 'Notificacoes'

---
+++ src/components/Notificacoes.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="notificacoes">
+        <article class="message is-success">
+            <div class="message-header">
+                Atenção!
+            </div>
+            <div class="message-body">
+                Notificação!
+            </div>
+        </article>
+        <article class="message is-warning">
+            <div class="message-header">
+                Atenção!
+            </div>
+            <div class="message-body">
+                Notificação!
+            </div>
+        </article>
+        <article class="message is-danger">
+            <div class="message-header">
+                Atenção!
+            </div>
+            <div class="message-body">
+                Notificação!
+            </div>
+        </article>
+    </div>
+</template>
+
+<script lang="ts">
+
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'NotificacoesVue'
+});
+
+</script>
+
+<style>
+    .notificacoes {
+        position: absolute;
+        right: 0;
+        width: 300px;
+        padding: 8px;
+        z-index: 1;
+    }
+</style>
